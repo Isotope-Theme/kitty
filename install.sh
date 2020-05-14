f777a7960301b733b1e3b14a44717a31d529916e
Arden Rasmussen <ardenisthebest@gmail.com>
2020-05-14 13:35:00 -0700

    Added light theme

--- install.sh
+++ install.sh
@@ -3,5 +3,24 @@
 if [[ ! -d "$HOME/.config/kitty/" ]]; then
   mkdir -p "$HOME/.config/kitty/"
 fi
-echo "Installing Isotope theme to $HOME/.config/kitty/kitty.conf"
-curl -s https://raw.githubusercontent.com/Isotope-Theme/kitty/master/isotope.conf >> $HOME/.config/kitty/kitty.conf
+VARIANTS=("dark" "light" "quit")
+select variant in "${VARIANTS[@]}"; do
+  case $variant in
+    "dark")
+      echo "Installing Isotope Dark theme to $HOME/.config/kitty/kitty.conf"
+      curl -s https://raw.githubusercontent.com/Isotope-Theme/kitty/master/isotope-dark.conf >> $HOME/.config/kitty/kitty.conf
+      break
+      ;;
+    "light")
+      echo "Installing Isotope Light theme to $HOME/.config/kitty/kitty.conf"
+      curl -s https://raw.githubusercontent.com/Isotope-Theme/kitty/master/isotope-light.conf >> $HOME/.config/kitty/kitty.conf
+      break
+      ;;
+    "quit")
+      break
+      ;;
+    *)
+      echo "invalid variant $REPLY"
+      ;;
+  esac
+done
